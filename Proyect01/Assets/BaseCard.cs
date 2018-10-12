b0ebf7f1daa66eb8cda4598913bdf48fb582c7f6
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2018-10-11 23:38:51 -0300

    Editor de cartas simple

--- Proyect01/Assets/BaseCard.cs
+++ Proyect01/Assets/BaseCard.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class BaseCard : MonoBehaviour {
 
     //o mesh renderer
-    public SpriteRenderer image;
+    public Texture2D illustration;
     List<string> CardType = new List<string>();
     public int life;
     public int energy;
-    public int atack;
+    public int attack;
     public int defense;
     //public List<Tuple<float, string>> PayCost;
-    public int manaCost;
+    public int mana;
     public int stars;
     public string description;
-    public SpriteRenderer backgroundCard;
-    public SpriteRenderer framework;
-    public string cardName;
+    public Texture2D background;
+    public Texture2D frame;
+    public Texture2D back;
+    public string cardname;
     public string autorSign;
+    public string cardtype;
+    public int cost;
+    public string ability;
 
 
     private void Start()
---
+++ Proyect01/Assets/Editor/CardCustomEditor.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BaseCard))]
+public class CardCustomEditor : Editor {
+
+    public BaseCard card;
+    public void OnEnable() {
+        card = (BaseCard) target;
+    }
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        CardName();
+        Images();
+        Stats();
+        Description();
+        //Como agregar iconos?
+    }
+
+    void CardName() {
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Nombre", GUILayout.Width(60));
+        card.cardname = EditorGUILayout.TextField(card.cardname);
+        EditorGUILayout.EndHorizontal();
+    }
+    void Images() {
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField("Marco", GUILayout.Width(80));
+        card.frame = (Texture2D) EditorGUILayout.ObjectField(card.frame, typeof(Texture2D), true);
+        //Aca va lo de mostrar la textura, pero no estoy segura como hacerlo xd 
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField("Fondo", GUILayout.Width(80));
+        card.background = (Texture2D) EditorGUILayout.ObjectField(card.background, typeof(Texture2D), true);
+        //Aca va lo de mostrar la textura, pero no estoy segura como hacerlo xd 
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField("Ilustracion", GUILayout.Width(80));
+        card.illustration = (Texture2D) EditorGUILayout.ObjectField(card.illustration, typeof(Texture2D), true);
+        //Aca va lo de mostrar la textura, pero no estoy segura como hacerlo xd 
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField("Reverso", GUILayout.Width(80));
+        card.back = (Texture2D) EditorGUILayout.ObjectField(card.back, typeof(Texture2D), true);
+        //Aca va lo de mostrar la textura, pero no estoy segura como hacerlo xd 
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndHorizontal();
+
+    }
+    void Stats() {
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Vida", GUILayout.Width(60));
+        card.life = EditorGUILayout.IntField(card.life);
+        EditorGUILayout.LabelField("Costo", GUILayout.Width(60));
+        card.cost = EditorGUILayout.IntField(card.cost);
+        EditorGUILayout.EndHorizontal();
+
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Energia", GUILayout.Width(60));
+        card.energy = EditorGUILayout.IntField(card.energy);
+        EditorGUILayout.LabelField("Mana", GUILayout.Width(60));
+        card.mana = EditorGUILayout.IntField(card.mana);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Ataque", GUILayout.Width(60));
+        card.attack = EditorGUILayout.IntField(card.attack);
+        EditorGUILayout.LabelField("Defensa", GUILayout.Width(60));
+        card.defense = EditorGUILayout.IntField(card.defense);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Estrellas", GUILayout.Width(60));
+        card.stars = EditorGUILayout.IntField(card.stars);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Tipo", GUILayout.Width(60));
+        card.cardtype = EditorGUILayout.TextField(card.cardtype);
+        EditorGUILayout.EndHorizontal();
+
+    }
+
+    void Description() {
+        EditorGUILayout.LabelField("Descripcion", GUILayout.Width(80));
+        card.description = EditorGUILayout.TextArea(card.description,GUILayout.Height(60));
+
+        EditorGUILayout.LabelField("Habilidad", GUILayout.Width(80));
+        card.ability = EditorGUILayout.TextArea(card.ability, GUILayout.Height(60));
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Firma", GUILayout.Width(60));
+        card.autorSign = EditorGUILayout.TextField(card.autorSign);
+        EditorGUILayout.EndHorizontal();
+
+    }
+
+}
